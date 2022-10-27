f47da00d77c655871d0483a2dfedc2d74e4c0a7e
Jul-JY <julietta198307@gmail.com>
2022-10-27 20:15:00 +0800

    Not Found file creation and functionality and CSS updates

---
+++ apps/client/src/pages/NotFound.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = ({ loading }) => {
+  // rendering our ui
+  if (loading) {
+    return (
+      <div className="loadingscreen">
+        <h2 className="mt-2" style={{ fontSize: "45px", color: "white" }}>
+          Loading...
+        </h2>
+        <br />
+      </div>
+    );
+  } else {
+    return (
+      <div className="loadingscreen">
+        <h2 className="mt-2" style={{ fontSize: "45px", color: "white" }}>
+          Page Not Found
+        </h2>
+        <br />
+        <Link to="/">Go to Home</Link>
+      </div>
+    );
+  }
+};
+
+export default NotFound;
