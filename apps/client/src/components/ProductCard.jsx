8d0a08d06076f260a15437a72833c0c68065b0aa
Jul-JY <julietta198307@gmail.com>
2022-11-02 18:17:07 +0800

    Product Card Componen:  add to cart button functionality and rendering

--- apps/client/src/components/ProductCard.jsx
+++ apps/client/src/components/ProductCard.jsx
@@ -21,37 +21,47 @@ const ProductCard = ({ product }) => {
   //! rendering our ui
   return (
     <>
-      {/*product card*/}
+      {/* product card */}
       <div className="card">
         <div className="card-image">
-          {/*product image*/}
-          <img src={product.images[0]}/>
+          {/* product image */}
+          <img src={product.images[0].url} alt={product.name} />
+          <div>
+            {/* product name */}
+            <span
+              onClick={() => gotoproduct(product._id)}
+              className="card-title producthead"
+              style={{
+                position: "absolute",
+                top: 200,
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+            >
+              {product.name}
+            </span>
+
+            {/* add to cart button */}
+            <span
+              onClick={() => add(product._id, 1)}
+              to="/"
+              className="btn-floating halfway-fab waves-effect waves-light #1E2639"
+              style={{ backgroundColor: "rgb(30, 38, 57)" }}
+            >
+              <i className="material-icons">+</i>
+            </span>
+          </div>
         </div>
-        {/*product name*/}
-        <span
-          className="card-title producthead"
-          style={{
-            position: "absolute",
-            top: 200,
-            textDecoration: "underline",
-            cursor: "pointer",
-          }}
-        >{product.name}</span>
-
-        {/*add to card button */}
-        <span></span>
-      </div>
-
-      <div className="card-content">
-        {/*p[roduct description */}
-        <p className="description">{product.description}</p>
-      </div>
-
-      <div>
-        {/* product price */}
-        <p>
-          <b>Price: $ {product.price}</b>
-        </p>
+        <div className="card-content">
+          {/* product description */}
+          <p className="description">{product.description}</p>
+          <div className="dialog mt-2">
+            {/* product price */}
+            <p>
+              <b>Price: $ {product.price}</b>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
