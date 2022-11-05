283820ce32b9bbac7e4e6e49d9d821da7b6c8fd2
Berwyn Tan <berwyntan@gmail.com>
2022-11-05 10:56:07 +0800

    update main.jsx

--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 //import { AppProvider } from "./context/UseAppContext";
 import "bootstrap/dist/css/bootstrap.min.css"; // imporing bootstrap css
 import "react-toastify/dist/ReactToastify.css"; // importing toastify css
-import "../node_modules/react-image-gallery/styles/css/image-gallery.css"; // importing product page slider css
+import "react-image-gallery/styles/css/image-gallery.css"; // importing product page slider css
 
 import { ToastContainer } from "react-toastify"; // imporing container for showing the toast message
 
