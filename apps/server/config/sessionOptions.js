4b9125b2aa84ab36df18994664c73ab06b45379f
Berwyn Tan <berwyntan@gmail.com>
2022-11-06 13:53:17 +0800

    server: sessionOptions

--- apps/server/config/sessionOptions.js
+++ apps/server/config/sessionOptions.js
@@ -4,7 +4,7 @@ const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { /*secure: true,*/ maxAge: 24 * 60 * 60 * 1000 },
+    cookie: { secure: true, maxAge: 24 * 60 * 60 * 1000 },
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URL})
 }
 
