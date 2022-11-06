763930a88285d650a81c5b73a1e8ce77459af5ef
Berwyn Tan <berwyntan@gmail.com>
2022-11-06 10:14:55 +0800

    server: sessionOptions

--- apps/server/config/sessionOptions.js
+++ apps/server/config/sessionOptions.js
@@ -2,7 +2,7 @@ const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    cookie: { secure: true, maxAge: 24 * 60 * 60 * 1000 },
 }
 
 module.exports = sessionOptions
